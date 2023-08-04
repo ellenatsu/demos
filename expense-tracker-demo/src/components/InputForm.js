535b8996ca310e61bf5bb968d8362a137a9c9ca6
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-08-04 14:40:25 -0700

    add state change monitor

--- expense-tracker-demo/src/components/InputForm.js
+++ expense-tracker-demo/src/components/InputForm.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const InputForm = () => {
+  const [selectCategory, setSelectCategory] = useState("grocery");
+
   const submitHandler = (event) => {
     event.preventDefault();
   };
 
   const resetHandler = () => {};
+
+  const inputChangeHandler = (input, value) => {};
+  const categoryChangeHandler = (event) => {
+    setSelectCategory(event.target.value);
+  };
+
+  const [formData, setFormData] = useState({
+    date: "",
+    text: "",
+    amount: "",
+  });
+
+  // Event handler for input elements
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   // Get the current date in the format YYYY-MM-DD
   const currentDate = new Date().toISOString().slice(0, 10);
@@ -17,15 +39,22 @@ export const InputForm = () => {
     <form className="form" onClick={submitHandler}>
       <div className="input-group">
         <p>
-          <label htmlFor="duration">Date</label>
-          <input type="date" id="duration" />
+          <label htmlFor="date">Date</label>
+          <input
+            onChange={handleInputChange}
+            type="date"
+            id="date"
+            value={formData.date}
+          />
         </p>
         <p>
           <label htmlFor="category">Category</label>
-          <select id="category">
-            <option value="grocery" selected>
-              Grocery
-            </option>
+          <select
+            id="category"
+            value={selectCategory}
+            onChange={categoryChangeHandler}
+          >
+            <option value="grocery">Grocery</option>
             <option value="food">Food</option>
             <option value="pet">Pet</option>
           </select>
@@ -43,7 +72,12 @@ export const InputForm = () => {
         </p>
         <p>
           <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" />
+          <input
+            type="number"
+            id="amount"
+            value={formData.amount}
+            onChange={handleInputChange}
+          />
         </p>
       </div>
       <p className="actions">
