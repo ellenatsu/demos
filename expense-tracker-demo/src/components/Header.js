bbafed5ed377f111f23b24aa4c628a53b998351a
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-08-09 14:53:16 -0700

    revise style

--- expense-tracker-demo/src/components/Header.js
+++ expense-tracker-demo/src/components/Header.js
@@ -4,7 +4,6 @@ import React from "react";
 export const Header = () => {
   return (
     <header className="header">
-      <img src={logo} alt="logo" />
       <h1>Budget Tracker</h1>
     </header>
   );
