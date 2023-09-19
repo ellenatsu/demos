f48529e9e5ebeae3610f3a28b8f919836b6dcfcd
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-09-19 15:00:57 -0700

    add some env

--- profolio-demo/src/components/Experience.jsx
+++ profolio-demo/src/components/Experience.jsx
@@ -1,20 +1,33 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
-import { useGLTF } from '@react-three/drei'
+import { useGLTF } from "@react-three/drei";
+import { useControls } from "leva";
 
 export const Experience = () => {
-  
+  const { animation } = useControls({
+    animation: {
+      value: "typing",
+      options: ["typing", "falling", "idle"],
+    },
+  });
   return (
     <>
       <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
-         <Avatar />
-      </group>
-      
+        <Avatar animation={animation} />
 
-     
-      
-      
+        <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+          <boxGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <mesh scale={5} rotation-x={-Math.PI * 0.5}>
+          <planeGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+      </group>
+
       <ambientLight intensity={1} />
     </>
   );
