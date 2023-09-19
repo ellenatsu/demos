fd525e8da848a12e0ab1afcefdcbbd3f9eb72de4
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-09-19 01:27:44 -0700

    add animations

--- profolio-demo/src/components/Experience.jsx
+++ profolio-demo/src/components/Experience.jsx
@@ -1,13 +1,21 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useGLTF } from '@react-three/drei'
 
 export const Experience = () => {
+  
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+         <Avatar />
+      </group>
+      
+
+     
+      
+      
+      <ambientLight intensity={1} />
     </>
   );
 };
